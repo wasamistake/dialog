5c9eeb38b622f9e7c5fa8f2747c2ffa80f68b385
wasamistake <150041252+wasamistake@users.noreply.github.com>
2023-11-09 16:23:23 -0300

    Set up component showcasing with `storybook`

---
+++ .storybook/preview.ts
@@ -0,0 +1,17 @@
+import type { Preview } from '@storybook/react'
+import './global.css'
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    docs: { toc: true },
+  },
+}
+
+export default preview
